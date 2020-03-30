befebb0d265646aadc286cbe70af64df285898df
LiuTao <821580467@qq.com>
2020-03-30 11:08:53 +0800

    提交资源文件

--- FakeProject/code/laya/p_yinyou/inner_project/src/UpdateProject.ts
+++ FakeProject/code/laya/p_yinyou/inner_project/src/UpdateProject.ts
@@ -13,11 +13,37 @@ class UpdateProject {
     constructor() {
         // D:\Work_Projects\ltg-laya\FakeProject\code\laya\p_yinyou
         let currentWorkPath = process.cwd();
+        console.log("开始更新框架内容");
+        this._CopyLaya(currentWorkPath);
+
+        console.log("更新框架完成");
+    }
+
+    private _CopyUnity(currentWorkPath: string) {
+        let projectName = LTUtils.GetDirName(currentWorkPath);
+        let srcPath = path.join(currentWorkPath, './others/publish/templates/_project/unity');
+        let targetPath = path.join(currentWorkPath, './../../unity/' + projectName + '/');
+
+        for (let value of CommonConfig.needCopyUnity) {
+            let combieSrc = path.join(srcPath, value);
+            let fileType = LTUtils.IsFileOrDir(combieSrc);
+            if (fileType == EFileType.NotExist) {
+                console.log(combieSrc, "不存在");
+                continue;
+            }
+            let combieTarget = path.join(targetPath, value);
+            if (fileType == EFileType.File) {
+                LTUtils.CopyFile(combieSrc, combieTarget);
+            } else {
+                LTUtils.CopyDir(combieSrc, combieTarget);
+            }
+            console.log("拷贝", combieSrc, "完成");
+        }
+    }
+
+    private _CopyLaya(currentWorkPath: string) {
         let srcPath = path.join(currentWorkPath, './others/publish/templates/_project/laya');
         let targetPath = currentWorkPath;
-
-        console.log("开始更新框架内容");
-
         for (let value of CommonConfig.needCopy) {
             let combieSrc = path.join(srcPath, value);
             let fileType = LTUtils.IsFileOrDir(combieSrc);
@@ -33,8 +59,6 @@ class UpdateProject {
             }
             console.log("拷贝", combieSrc, "完成");
         }
-
-        console.log("更新框架完成");
     }
 
 }
