a73f5ee585ae1f696319720db0da6f3402b865c5
LiuTao <821580467@qq.com>
2020-04-24 17:00:36 +0800

    扩充平台

---
+++ FakeProject/code/laya/p_yinyou/src/LTGame/Platform/Impl/Web/WebRecordManager.ts
@@ -0,0 +1,21 @@
+import DefaultRecordManager from "../../DefaultRecordManager";
+
+export class WebRecordManager extends DefaultRecordManager {
+
+    supportRecord = false;
+
+    ShareVideo(onSuccess: Laya.Handler, onCancel: Laya.Handler, onFailed: Laya.Handler) {
+        if (this.supportRecord) {
+            console.log("强制模拟成功");
+            if (onSuccess) {
+                onSuccess.run();
+            }
+        } else {
+            console.log("强制模拟失败");
+            if (onFailed) {
+                onFailed.run();
+            }
+        }
+
+    }
+}
