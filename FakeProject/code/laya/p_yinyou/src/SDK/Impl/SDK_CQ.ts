fc12c458df60e0e5aca365f603062d0d6ab13c0b
tflyq <tian1991feng@sina.cn>
2020-05-28 14:29:21 +0800

    工作日判定（排除法定休息日）

--- FakeProject/code/laya/p_yinyou/src/SDK/Impl/SDK_CQ.ts
+++ FakeProject/code/laya/p_yinyou/src/SDK/Impl/SDK_CQ.ts
@@ -1,12 +1,12 @@
 import { CommonEventId } from "../../LTGame/Commom/CommonEventId";
 import LTHttp from "../../LTGame/Net/LTHttp";
+import { EPlatformType } from "../../LTGame/Platform/EPlatformType";
+import LTPlatform from "../../LTGame/Platform/LTPlatform";
 import { ShareInfo } from "../../LTGame/Platform/ShareInfo";
 import ShareManager from "../../LTGame/Platform/ShareManager";
+import { ECheckState } from "../common/ECheckState";
 import SDKADManager from "../SDKADManager";
 import SDK_Default from "./SDK_Default";
-import { ECheckState } from "../common/ECheckState";
-import LTPlatform from "../../LTGame/Platform/LTPlatform";
-import { EPlatformType } from "../../LTGame/Platform/EPlatformType";
 
 export default class SDK_CQ extends SDK_Default {
 
@@ -19,13 +19,14 @@ export default class SDK_CQ extends SDK_Default {
     adManager: SDKADManager;
     uid: string = "sdk_test";
     enableDebug: boolean = true;
-
+    dateInfo: DateInfo[] = [];
 
     private _headPrefix = "https://gamer.api.gugudang.com";
 
     Init(flg: string, channel: string, controlVersion: string, appId: string) {
         super.Init(flg, channel, controlVersion, appId);
         this._RequestShareInfo();
+        this._RequestSelfDateInfo();
     }
 
     private _RequestShareInfo() {
@@ -58,7 +59,19 @@ export default class SDK_CQ extends SDK_Default {
             console.error("分享接口返回错误", getObj);
         }
     }
-
+    /**CDN 节假日信息配置 年底需更新次年数据 */
+    private _RequestSelfDateInfo() {
+        LTHttp.Send(`https://hs.yz061.com/res/down/public/configs/Dates.json`, Laya.Handler.create(this, this.onGetDatesInfo),
+            Laya.Handler.create(this, this.onGetDatesError), true);
+    }
+    onGetDatesError(res: string) {
+        console.error('云 获取日历信息失败', res);
+    }
+    onGetDatesInfo(res: string) {
+        let days = JSON.parse(res);
+        this.dateInfo = days as DateInfo[];
+        console.log('云 获取日历信息', this.dateInfo);
+    }
     RecordShowAd(adList: SDK.ADRecordShowData[]) {
         console.log("功能暂未实现");
     }
@@ -120,22 +133,31 @@ export default class SDK_CQ extends SDK_Default {
                 } else {
                     this.checkState = LTPlatform.instance.platform == EPlatformType.Oppo ? ECheckState.InCheck : ECheckState.Normal;
                 }
-                //工作时间屏蔽
-                let nowtime = new Date();
-                let h = nowtime.getHours();
-                let timeArr: string[] = [];
-                if (result['nowtime']) {
-                    nowtime = new Date(result['nowtime']);
-                    h = nowtime.getHours();
-                }
-                if (result['shieldHours']) {
-                    timeArr = result['shieldHours'].split(',');
-                }
-                console.error(timeArr)
                 if (this.checkState == ECheckState.NoGame) {
-                    if (timeArr.indexOf(h.toString()) >= 0) {
-                        console.log('校准时间', timeArr, h);
+                    //工作时时段屏蔽
+                    let nowtime = new Date();
+                    let h = nowtime.getHours();
+                    let date = nowtime.toLocaleDateString();
+
+                    let shieldHours: string[] = [];
+                    if (result['nowtime']) {
+                        nowtime = new Date(result['nowtime']);
+                        h = nowtime.getHours();
+                    }
+                    if (result['shieldHours']) {
+                        shieldHours = result['shieldHours'].split(',');
+                    }
+                    let today = this.dateInfo.filter(e => e.date == date);
+                    let isWorkday = true;
+                    if (today && today.length) {
+                        isWorkday = today[0].type == 0;//type：0 工作日 1 周末  2 节假日 
+                    }
+                    //工作  时段  
+                    if (isWorkday && shieldHours.indexOf(h.toString()) >= 0) {
+                        console.log('工作', shieldHours, h);
                         this.checkState = ECheckState.Normal;
+                    } else {
+                        console.log('休息', date, h);
                     }
                 }
             } else {
@@ -146,6 +168,8 @@ export default class SDK_CQ extends SDK_Default {
             // 失败
             console.error("云控消息返回失败", res);
         }
+
+
 
         console.log("云控版本为:", this.controlVersion, "config:", this.isConfigEnable, `广告开关:${this.isADEnable}, 审核状态:${ECheckState[this.checkState]},误触概率:${this.payRate},屏蔽状态:${this.isShielding},延迟按钮:${this.isDelayClose}`);
         if (this.controlVersion) {
@@ -200,4 +224,8 @@ export default class SDK_CQ extends SDK_Default {
         console.log("功能暂未实现");
     }
 
+}
+export interface DateInfo {
+    date: string;
+    type: number;
 }
