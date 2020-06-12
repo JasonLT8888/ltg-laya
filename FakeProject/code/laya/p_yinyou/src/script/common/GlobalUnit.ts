d40e548db35e1f9d8333046808bda606d62d7821
LiuTao <821580467@qq.com>
2020-06-12 09:37:23 +0800

    增加s3d自动存Window

--- FakeProject/code/laya/p_yinyou/src/script/common/GlobalUnit.ts
+++ FakeProject/code/laya/p_yinyou/src/script/common/GlobalUnit.ts
@@ -5,6 +5,7 @@ export default class GlobalUnit {
 
     static InitAll() {
         this.s3d = Laya.stage.addChildAt(new Laya.Scene3D(), 0) as Laya.Scene3D;
+        window['s3d'] = this.s3d;
     }
 
 }
