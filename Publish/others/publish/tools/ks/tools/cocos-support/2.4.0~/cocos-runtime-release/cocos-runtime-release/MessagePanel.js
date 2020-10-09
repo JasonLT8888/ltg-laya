f915b7d4c2bd2a12ddc7324b16793847c07bf1b4
LiuTao <821580467@qq.com>
2020-10-09 10:24:10 +0800

    发布框架

--- Publish/others/publish/tools/ks/tools/cocos-support/2.4.0~/cocos-runtime-release/cocos-runtime-release/MessagePanel.js
+++
@@ -1,1 +0,0 @@
-Editor.Panel.extend({ style: `:host{margin:5px;}h2{color:#f90;}`, template: `<h2>构建 CPK 信息</h2><hr/><div><span id="label"style="color:red;font-size:16px;"></span></div>`, $: { label: '#label', }, run(argv) { this.$label.innerText = argv; }, });
