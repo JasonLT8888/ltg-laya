27d066483ebf7eae0f0fa1040a5bd43584c564d8
tflyq <tian1991feng@sina.cn>
2021-06-07 15:38:25 +0800

    转盘

--- Publish/others/publish/templates/_project/laya/src/LTG_CommonUI/UI/LTCom/LTG_UI_view_roll.ts
+++ Publish/others/publish/templates/_project/laya/src/LTG_CommonUI/UI/LTCom/LTG_UI_view_roll.ts
@@ -1,13 +1,15 @@
 /** This is an automatically generated class by FairyGUI. Please do not modify it. **/
 
 import LTG_UI_view_roll_panel from "./LTG_UI_view_roll_panel";
+import LTG_UI_common_ad_btn from "./LTG_UI_common_ad_btn";
+import LTG_UI_btn_refuse from "./LTG_UI_btn_refuse";
 
 export default class LTG_UI_view_roll extends fgui.GComponent {
 
 	public m_view_roll:LTG_UI_view_roll_panel;
 	public m_progress_supper:fgui.GProgressBar;
-	public m_btn_watchad:fgui.GButton;
-	public m_btn_freeget:fgui.GButton;
+	public m_btn_watchad:LTG_UI_common_ad_btn;
+	public m_btn_freeget:LTG_UI_btn_refuse;
 	public m_text_notice:fgui.GTextField;
 
 	public static URL:string = "ui://hbq27te38gel42";
@@ -21,10 +23,10 @@ export default class LTG_UI_view_roll extends fgui.GComponent {
 	}
 
 	protected onConstruct(): void {
-		this.m_view_roll = <LTG_UI_view_roll_panel><any>(this.getChildAt(0));
+		this.m_view_roll = <LTG_UI_view_roll_panel><any>(this.getChildAt(1));
 		this.m_progress_supper = <fgui.GProgressBar><any>(this.getChildAt(3));
-		this.m_btn_watchad = <fgui.GButton><any>(this.getChildAt(5));
-		this.m_btn_freeget = <fgui.GButton><any>(this.getChildAt(6));
+		this.m_btn_watchad = <LTG_UI_common_ad_btn><any>(this.getChildAt(5));
+		this.m_btn_freeget = <LTG_UI_btn_refuse><any>(this.getChildAt(6));
 		this.m_text_notice = <fgui.GTextField><any>(this.getChildAt(7));
 	}
 }
