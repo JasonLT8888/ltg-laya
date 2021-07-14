96c6cc8893a13959c48181db9e5b25a1b767189a
LiuTao <821580467@qq.com>
2021-07-14 11:12:37 +0800

    GameData.ts MainStart.ts DataManager.ts     修改数据初始化 LTLineCurve.ts     修改接口

--- FakeProject/code/laya/p_yinyou/src/LTGame/LTUtils/LTLineCurve.ts
+++ FakeProject/code/laya/p_yinyou/src/LTGame/LTUtils/LTLineCurve.ts
@@ -13,21 +13,52 @@ export class LTLineCurve {
     private _unitProgress: number;
     public totalLength: number;
 
-    constructor(keys: number[], posList: Laya.Vector3[], rotList: Laya.Quaternion[]) {
-        this._keys = ArrayEx.Copy(keys);
-        this._posList = ArrayEx.Copy(posList);
-        this._rotList = ArrayEx.Copy(rotList);
+    constructor() {
+        this._keys = [];
+        this._posList = [];
+        this._rotList = [];
+        this._unitProgress = 1;
+    }
+
+    public PushDatas(keys: number[], posList: Laya.Vector3[], rotList: Laya.Quaternion[]) {
+        this._keys.push(...keys)
+        this._posList.push(...posList);
+        this._rotList.push(...rotList);
         this._unitProgress = 1 / this._keys.length;
     }
 
-    public EvaluatePos(progress: number): Laya.Vector3 {
-        let indexProgress = progress * this._keys.length;
-        let preIndex = Math.floor(indexProgress);
-        let behindIndex = preIndex + 1;
-        let lerpProgress = (progress - (preIndex * this._unitProgress)) / this._unitProgress;
+    public EvaluatePos(distance: number, lastIndex: number = 0): [Laya.Vector3, number] {
+        if (distance <= 0) return [this._posList[0].clone(), 0];
+        if (distance >= this.totalLength) return [this._posList[this._posList.length - 1].clone(), this._keys.length];
+        let searchIndex = Math.max(lastIndex - 5, 0);
+        for (; searchIndex < this._keys.length; ++searchIndex) {
+            if (this._keys[searchIndex] > distance) {
+                break;
+            }
+        }
+        let preIndex = Math.max(searchIndex - 1, 0);
+        let behindIndex = Math.max(searchIndex, 1);
+        let lerpProgress = (distance - this._keys[preIndex]) / (this._keys[behindIndex] - this._keys[preIndex]);
         let prePos = this._GetPos(preIndex);
         let behindPos = this._GetPos(behindIndex);
-        return Vector3Ex.Lerp(prePos, behindPos, lerpProgress);
+        return [Vector3Ex.Lerp(prePos, behindPos, lerpProgress), preIndex];
+    }
+
+    public EvaluateRot(distance: number, lastIndex: number = 0): Laya.Quaternion {
+        if (distance <= 0) return this._rotList[0].clone();
+        if (distance >= this.totalLength) return this._rotList[this._rotList.length - 1].clone();
+        let searchIndex = Math.max(lastIndex - 5, 0);
+        for (; searchIndex < this._keys.length; ++searchIndex) {
+            if (this._keys[searchIndex] > distance) {
+                break;
+            }
+        }
+        let preIndex = Math.max(searchIndex - 1, 0);
+        let behindIndex = Math.max(searchIndex, 1);
+        let lerpProgress = (distance - this._keys[preIndex]) / (this._keys[behindIndex] - this._keys[preIndex]);
+        let preRot = this._GetRot(preIndex);
+        let behindRot = this._GetRot(behindIndex);
+        return QuaternionEx.Lerp(preRot, behindRot, lerpProgress);
     }
 
     private _GetPos(index: number): Laya.Vector3 {
@@ -40,16 +71,6 @@ export class LTLineCurve {
         if (index < 0) return this._rotList[0];
         if (index > this._rotList.length - 1) return this._rotList[this._rotList.length - 1];
         return this._rotList[index];
-    }
-
-    public EvaluateRot(progress: number): Laya.Quaternion {
-        let indexProgress = progress * this._keys.length;
-        let preIndex = Math.floor(indexProgress);
-        let behindIndex = preIndex + 1;
-        let lerpProgress = (progress - (preIndex * this._unitProgress)) / this._unitProgress;
-        let preRot = this._GetRot(preIndex);
-        let behindRot = this._GetRot(behindIndex);
-        return QuaternionEx.Lerp(preRot, behindRot, lerpProgress);
     }
 
 }
